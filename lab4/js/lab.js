6bc013fd324d0b9df4b2ae6fb4ba67ef85c7a724
scapetan <ms.capetanakis@gmail.com>
2024-10-15 19:05:48 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - These are the tasks for rock paper scissors!
+// Author: Stella Capetanakis
+// Date: October 15 2024
 
 // Constants
 
@@ -9,6 +9,16 @@
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
+  //COMMENTS FOR HOW TO BUILD COMPUTER GAME
+  //Rock Paper Scissors:
+//Give the first user 3 options to pick from: Rock, Paper, or Scissors.
+  //Save this answer
+//Give the second user the same 3 options to pick from.
+  //Save this answer
+//Compare the two saved answers against the stored rule conditions
+//If tie, repeat
+//If there is a winner announce the user with the winning task.
+
   // return results;
 }
 
