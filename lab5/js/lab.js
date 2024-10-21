8fa3417200c59ca57238974049a3a6d6cdbe440f
scapetan <ms.capetanakis@gmail.com>
2024-10-21 16:48:24 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,34 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - lab 5!!
+/*
+ * Author: Stella Capetanakis
+ * Date: Oct 21 2024
+*/
+// Variants
+make = "subaru";
+model = "impreza";
+color = "silver";
+year = "2018";
 
-// Constants
+//calculate age
+age = 2024 - year
 
 // Functions
+//no functions in lab 5
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//output
+document.writeln("make: " + make + "<br>");
+document.writeln("model: " + model + "<br>");
+document.writeln("color: " + color + "<br>");
+document.writeln("year: " + year + "<br>");
+document.writeln("age: " + age + "years<br>");
 
-// let's get this party started
-main();
+
+
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+// }
+
+// // let's get this party started
+// main();
