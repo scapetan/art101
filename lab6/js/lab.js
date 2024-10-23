63ce98a5de7a2d3bf5030b78e8130b285a4dbec3
scapetan <ms.capetanakis@gmail.com>
2024-10-22 18:50:16 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,41 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Stella Capetanakis
+// Date: October 22 2024
 
-// Constants
+// Variables
+myTransport = ["car", "bus", "walking"]
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+myMainRide = {
+  make: "Subaru",
+  model: "Impreza",
+  color: "Silver",
+  year: 2018,
+    //calculate age
+  age: function () {
+        return 2024-this.year;
+    }
 }
+//output
+document.writeln("My Transports: " + myTransport + "<br>");  
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+
+//object to doc
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+// // Functions
+
+// // this is an example function and this comment tells what it doees and what parameters are passed to it.
+// function myFunction(param1, param2) {
+//   // some code here
+//   // return results;
+// }
+
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+// }
+
+// // let's get this party started
+// main();
