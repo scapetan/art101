425d3b6705c980ecaa8765ff7f8cbff193f23031
scapetan <ms.capetanakis@gmail.com>
2024-10-24 21:05:57 -0700

    lab 7

---
+++ lab7/lab.js
@@ -0,0 +1,25 @@
+// index.js - javascript function creation
+// Author: Stella Capetanakis
+// Date: October 24 2024
+/*
+
+
+//function
+
+
+
+// // Functions
+
+// // this is an example function and this comment tells what it doees and what parameters are passed to it.
+// function myFunction(param1, param2) {
+//   // some code here
+//   // return results;
+// }
+
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+// }
+
+// // let's get this party started
+// main()*/
