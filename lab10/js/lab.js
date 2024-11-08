d19e996f4a90c9d215566a09279430595eccaaf7
scapetan <ms.capetanakis@gmail.com>
2024-11-07 23:55:35 -0800

    lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -7,22 +7,21 @@
 */
 
 function generateRandomText() {
-    const text = "blahblahblahblehblahblahhhh";
-    const min = 3;
-    const max = 100;
-    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-    // Get a random starting index to slice the blah text
-    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-    // Generate the random blah text
-    return text.slice(randStart, randStart + randLen);
-  }
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the blah text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem text
+  return text.slice(randStart, randStart + randLen);
+}
 
-  // click listener for button
+// click listener for button
 $("#make-convo").click(function(){
+  // get new fake dialogue
+  const newText = generateRandomText();
+  // append a new div to our output div
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+
 });
-
-// get new fake dialogue
-const newText = generateRandomText();
-
-// append a new div to our output div
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
